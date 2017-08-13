23c9fca7a34d6e128674f9a5f8171d1e83027a5d
nonphatic <afnl686@gmail.com>
2017-08-13 11:21:21 -0700

    Problem 81

---
+++ 81.hs
@@ -0,0 +1,17 @@
+import Data.List.Split
+
+getInitialLine :: [Integer] -> [Integer]
+getInitialLine firstLine = tail $ scanl (+) 0 firstLine
+
+getNextLine :: [Integer] -> [Integer] -> [Integer]
+getNextLine prevLine currLine =
+    tail $ scanl minSum (head prevLine) (zip prevLine currLine)
+    where minSum currSum (up, left) = min (left + up) (left + currSum)
+
+main :: IO ()
+main = do 
+    contents <- readFile "p081_matrix.txt"
+    let fileLines = lines contents
+    let matrix = map (map read . (splitOn ",")) fileLines :: [[Integer]]
+    let initialLine = getInitialLine $ head matrix
+    print $ last $ foldl getNextLine initialLine $ tail matrix
