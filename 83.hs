36178b200ce3ff2a6b41859c31c83b1aa29315ac
nonphatic <afnl686@gmail.com>
2017-08-17 22:29:21 -0700

    Problem 83: technically correct solution, but very slow...

--- 83.hs
+++ 83.hs
@@ -9,14 +9,15 @@ type Position = (Int, Int)
 data Element = Element Value IsVisited Position deriving (Eq, Show)
 
 instance Monoid Element where
-    mempty = Element 0 Visited (0, 0)
+    mempty = Element 0 Unvisited (0, 0)
     e `mappend` Element _ Unvisited _ = e
     Element _ Unvisited _ `mappend` e = e
     Element v1 Visited p1 `mappend` Element v2 Visited p2 =
         case getMin v1 v2 of 
             0  -> mempty
-            v1 -> Element v1 Visited p1
-            v2 -> Element v2 Visited p2
+            v -> if v == v1 
+                then Element v1 Visited p1
+                else Element v2 Visited p2
 
 getMin :: Value -> Value -> Value
 getMin v1 0  = v1
@@ -25,11 +26,11 @@ getMin v1 v2 = min v1 v2
 
 getUnvisitedNeighbours :: Matrix Element -> Element -> [Element]
 getUnvisitedNeighbours m (Element _ _ (i, j)) =
-    filter (\(Element _ isVisited _) -> isVisited == Visited)
-        (catMaybes [safeGet (i - 1) j      m
-                   ,safeGet (i + 1) j      m
-                   ,safeGet  i     (j - 1) m
-                   ,safeGet  i     (j + 1) m])
+    filter (\(Element _ isVisited _) -> isVisited == Unvisited)
+        (catMaybes [safeGet (i - 1) j      m,
+                    safeGet (i + 1) j      m,
+                    safeGet  i     (j - 1) m,
+                    safeGet  i     (j + 1) m])
 
 getNeighbourDistances :: Element -> [Element] -> [Element]
 getNeighbourDistances e = map (addValue e)
@@ -39,10 +40,6 @@ dijkstraMatrix :: Matrix Element -> Element
 dijkstraMatrix m = 
     let neighbourDistanceMatrix = fmap (\e -> getNeighbourDistances e (getUnvisitedNeighbours m e)) m
     in fold (fmap fold neighbourDistanceMatrix)
-    -- for unvisited element in matrix
-        -- for unvisited neighbour of element
-            -- if element value + neighbour value < min then set as min
-    -- return min; default is (0, (0, 0))
 
 nextMatrix :: Matrix Element -> Matrix Element
 nextMatrix m =
@@ -50,6 +47,14 @@ nextMatrix m =
         maybeMatrix = safeSet (Element minValue Visited position) position m
     in case maybeMatrix of 
         Just next -> next
+        Nothing -> m
+
+setInitial :: Matrix Element -> Matrix Element
+setInitial m =
+    let Element v _ _ = m ! (1, 1)
+        maybeMatrix = safeSet (Element v Visited (1, 1)) (1, 1) m
+    in case maybeMatrix of 
+        Just mm -> mm 
         Nothing -> m
 
 initElement :: Matrix Integer -> Position -> Element
@@ -61,10 +66,18 @@ toElementMatrix :: Matrix Integer -> Matrix Element
 toElementMatrix m =
     matrix (nrows m) (ncols m) (initElement m)
 
+findShortestPathLength :: Matrix Element -> Value
+findShortestPathLength m =
+    let Element v isVisited _ = m ! (nrows m, ncols m)
+    in case isVisited of 
+        Visited -> v
+        Unvisited -> findShortestPathLength $ nextMatrix m
+
 main :: IO ()
 main = do
-    contents <- readFile "p081_matrix.txt"
+    contents <- readFile "p083_matrix.txt"
     let listsMatrix = map (map read . (splitOn ",")) $ lines contents :: [[Integer]]
         valueMatrix = fromLists listsMatrix
-        mtrx = toElementMatrix valueMatrix
-    print $ nextMatrix mtrx
+        unvisitedMatrix = toElementMatrix valueMatrix
+        mtrx = setInitial unvisitedMatrix
+    print $ findShortestPathLength mtrx
